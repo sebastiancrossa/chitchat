c11df891c655493c2b4d0b89752e5bea2a23dec7
Sebastian Crossa <crossasebastian@gmail.com>
2020-01-04 01:03:13 -0700

    Basic initalization for server.js

---
+++ server/server.js
@@ -0,0 +1,13 @@
+// Libraries
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const cors = require("cors");
+
+// Initialization
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
