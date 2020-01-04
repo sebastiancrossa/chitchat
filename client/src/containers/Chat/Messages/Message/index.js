722ce2403f219944fb06e0e63c4419a3db9e8a65
Sebastian Crossa <crossasebastian@gmail.com>
2020-01-04 11:12:48 -0700

    React Emojify implementado a los mensajes

--- client/src/containers/Chat/Messages/Message/index.js
+++ client/src/containers/Chat/Messages/Message/index.js
@@ -3,9 +3,18 @@ import React from "react";
 import ReactEmoji from "react-emoji";
 
 const Message = ({ message, name }) => {
-  return (
+  let sentByCurrentUser = false;
+  const trimmedName = name.trim().toLowerCase();
+
+  if (message.user === trimmedName) sentByCurrentUser = true;
+
+  return sentByCurrentUser ? (
     <div>
-      <p>{message.text}</p>
+      <p>{ReactEmoji.emojify(message.text)}</p>
+    </div>
+  ) : (
+    <div>
+      <p>{ReactEmoji.emojify(message.text)}</p>
       <p>{message.user}</p>
     </div>
   );
