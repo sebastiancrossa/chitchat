a0c4e193e0678f73f42cccddac3428f4affaf37e
Sebastian Crossa <crossasebastian@gmail.com>
2020-01-04 01:11:10 -0700

    Backend route setup and socket.io initialization

--- server/server.js
+++ server/server.js
@@ -4,10 +4,24 @@ const socketio = require("socket.io");
 const http = require("http");
 const cors = require("cors");
 
+const router = require("./router");
+
 // Initialization
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 const PORT = process.env.PORT || 5000;
+
+// SocketIO
+io.on("connection", socket => {
+  console.log("New connection --");
+
+  socket.on("disconnect", () => {
+    console.log("Connection left --");
+  });
+});
+
+// Middleware
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
